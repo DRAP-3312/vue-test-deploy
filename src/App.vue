452af5a5d22abffa33f5d79121687e4c9da78f79
DRAP-3312 <davidake117@gmail.com>
2025-06-08 11:35:20 -0500

    test deploy 8

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Test deploy web by DRAP" />
+  <HelloWorld msg="Test deploy vue web by DRAP" />
 </template>
 
 <style scoped>
